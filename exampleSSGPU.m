15aec41ef9879bd91c1cc051acb33636fde452d6
David Fuentes <fuentesdt@gmail.com>
2014-10-11 03:14:14 -0500

    ENH: single solve

--- exampleSSGPU.m
+++ exampleSSGPU.m
@@ -3,8 +3,10 @@ close all
 format shortg
 
 % create npixel^3 image
-npixel   = 256;
-materialID = 10*int32(phantom3d('Modified Shepp-Logan',npixel));
+npixel   = 100;
+materialID = int32(10*phantom3d('Modified Shepp-Logan',npixel));
+materialID(materialID == 3  ) = 1;
+materialID(materialID == 10 ) = 3;
 imshow(materialID(:,:,npixel/2))
 
 % query the device
@@ -13,24 +15,25 @@ numSMs = deviceInfo.MultiprocessorCount;
 % % GPU must be reset on out of bounds errors
 % reset(gpuDevice(1))
 
+spacingX = 1.0e-3;
+spacingY = 1.0e-3;
+spacingZ = 1.0e-3;
+
 %%diary
 ntissue = 4;
 perfusion  = [5.e01 , 4.e01 , 3.e01, 6.e01];
 conduction = [5.e-1 , 4.e-1 , 3.e-1, 6.e-1];
 mueff      = [5.e02 , 4.e02 , 3.e02, 6.e02];
 nsource    = 10;
-xloc       = linspace(1,nsource ,nsource );
-yloc       = linspace(1,nsource ,nsource );
-zloc       = linspace(1,nsource ,nsource );
+xloc       = spacingX*linspace(1,nsource ,nsource )+1.e-4;
+yloc       = spacingY*linspace(1,nsource ,nsource )+1.e-4;
+zloc       = spacingZ*linspace(1,nsource ,nsource )+1.e-4;
 u_artery   = 37.;
 c_blood    = 3480.;
 power      = 10.;
 
 % initialize data on host
 h_temperature     = zeros(npixel,npixel,npixel);
-spacingX = 1.0;
-spacingY = 1.0;
-spacingZ = 1.0;
 % perform ONE transfer from host to device
 d_temperature  = gpuArray( h_temperature  );
 
@@ -48,4 +51,6 @@ ssptx.GridSize=[numSMs*32               1];
 h_temperature  = gather( d_temperature  );
 
 %%  plot
-%imagesc(h_temperature(:,:,100));
+imagesc(h_temperature(:,:,5), [37 100]);
+colormap default
+colorbar
--- steadyStatePennesLaser.cu
+++ steadyStatePennesLaser.cu
@@ -10,19 +10,20 @@ void pointSource(double r, double R1, double R2, double w, double k, double muef
    double PI_Var = 3.141592653589793;
    *temperature = ua+(P*PI_Var*(mueff*mueff)*exp(-mueff*r)*(1.0/4.0))/(r*(w-k*(mueff*mueff)))-(exp(-R1*mueff-R2*mueff)*exp(r*sqrt(w/k))*(P*PI_Var*(mueff*mueff)*exp(R1*sqrt(w/k))*exp(R2*mueff)-P*PI_Var*(mueff*mueff)*exp(R2*sqrt(w/k))*exp(R1*mueff)-P*PI_Var*R2*(mueff*mueff*mueff)*exp(R2*sqrt(w/k))*exp(R1*mueff)-R1*u0*w*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0+R1*ua*w*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0+R1*k*(mueff*mueff)*u0*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0-R1*k*(mueff*mueff)*ua*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0+P*PI_Var*R2*(mueff*mueff)*exp(R1*sqrt(w/k))*exp(R2*mueff)*sqrt(w/k)-R1*R2*u0*w*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0+R1*R2*ua*w*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0+R1*R2*k*(mueff*mueff)*u0*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0-R1*R2*k*(mueff*mueff)*ua*exp(R1*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0)*(1.0/4.0))/(r*(w-k*(mueff*mueff))*(exp(R1*sqrt(w/k)*2.0)-exp(R2*sqrt(w/k)*2.0)+R2*exp(R1*sqrt(w/k)*2.0)*sqrt(w/k)+R2*exp(R2*sqrt(w/k)*2.0)*sqrt(w/k)))-(exp(R1*sqrt(w/k))*exp(R2*sqrt(w/k))*exp(-r*sqrt(w/k))*exp(-R1*mueff)*exp(-R2*mueff)*(P*PI_Var*(mueff*mueff)*exp(R1*sqrt(w/k))*exp(R1*mueff)-P*PI_Var*(mueff*mueff)*exp(R2*sqrt(w/k))*exp(R2*mueff)+P*PI_Var*R2*(mueff*mueff*mueff)*exp(R1*sqrt(w/k))*exp(R1*mueff)+R1*u0*w*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0-R1*ua*w*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0-R1*k*(mueff*mueff)*u0*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0+R1*k*(mueff*mueff)*ua*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*4.0+P*PI_Var*R2*(mueff*mueff)*exp(R2*sqrt(w/k))*exp(R2*mueff)*sqrt(w/k)-R1*R2*u0*w*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0+R1*R2*ua*w*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0+R1*R2*k*(mueff*mueff)*u0*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0-R1*R2*k*(mueff*mueff)*ua*exp(R2*sqrt(w/k))*exp(R1*mueff)*exp(R2*mueff)*sqrt(w/k)*4.0)*(1.0/4.0))/(r*(w-k*(mueff*mueff))*(exp(R1*sqrt(w/k)*2.0)-exp(R2*sqrt(w/k)*2.0)+R2*exp(R1*sqrt(w/k)*2.0)*sqrt(w/k)+R2*exp(R2*sqrt(w/k)*2.0)*sqrt(w/k)));
 }
-// __device__
-// void WriteSolution(PetscComplex a[][MaxSize],int n,PetscComplex *x)
-// {
-//    int j,k;
-// 
-//    for (j=0;j<n;j++) {
-//       for (k=0;k<n+1;k++) {
-//          printf("%d %d %12.5e %12.5e ",k,j,a[k][j].real(),a[k][j].imag());
-//       }
-//       printf(" | %d  %12.5e %12.5e \n",j,x[j].real(),x[j].imag());
-//    }
-//    printf("\n");
-// }
+__device__
+void DebugWrite(int idx,double rad,double omega, double conduction, double mueff,double temp)
+{
+   printf("%d %12.5e %12.5e %12.5e %12.5e %12.5e\n",idx,rad,omega,conduction,mueff,temp);
+   //int j,k;
+
+   //for (j=0;j<n;j++) {
+   //   for (k=0;k<n+1;k++) {
+   //      printf("%d %d %12.5e %12.5e ",k,j,a[k][j].real(),a[k][j].imag());
+   //   }
+   //   printf(" | %d  %12.5e %12.5e \n",j,x[j].real(),x[j].imag());
+   //}
+   //printf("\n");
+}
 
 /*
  * Device code
@@ -30,7 +31,7 @@ void pointSource(double r, double R1, double R2, double w, double k, double muef
 __global__ 
 void steadyStatePennesLaser(
          int const NTissue,
-         const    int* MaterialID, // FIXME - is this data type converted correctly ? 
+         const    int* MaterialID,
          const double* Perfusion,
          const double* ThermalConduction,
          const double* EffectiveAttenuation,
@@ -85,10 +86,12 @@ void steadyStatePennesLaser(
                            + (jjj * SpacingY - SourceYloc[lll])*(jjj * SpacingY - SourceYloc[lll])
                            + (kkk * SpacingZ - SourceZloc[lll])*(kkk * SpacingZ - SourceZloc[lll]);
            double radius   = sqrt(radiusSQ);
-           // FIXME - add GF code here
+           // call GF code 
            double sourcetemperature;
            pointSource(radius, innerRadius, outerRadius, omega , conduction , mueff, InitialTemperature, ArterialTemperature, Power , &sourcetemperature);
-           temperature = temperature + sourcetemperature; 
+           // DebugWrite(idx,radius,omega,conduction,mueff,sourcetemperature);
+           // superposition
+           temperature = temperature + sourcetemperature/((double)NSource); 
           }
         // store temperature in array
         d_TemperatureArray[idx] = temperature;
