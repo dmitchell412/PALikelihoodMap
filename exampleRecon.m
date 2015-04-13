c497cc0a61fb00d73ab75d7a2586ff873c4656ca
David Fuentes <fuentesdt@gmail.com>
2015-04-12 22:10:57 -0500

    ENH: Variance Added

--- exampleRecon.m
+++ exampleRecon.m
@@ -3,15 +3,25 @@ clear all
 close all
 format shortg
 
-%% Simulate disjoint material/tissue types
-% create npixel^3 image
-npixel   = 100;
-materialID = int32(10*phantom3d('Modified Shepp-Logan',npixel));
-materialID(materialID == 3  ) = 1;
-materialID(materialID == 10 ) = 3;
+%% load tmp data structure
+prostatedata =load_untouch_nii('prostateAxT2.nii.gz');
+
+%% GMM tissue types
+prostatelabel =load_untouch_nii('prostateLabel.nii.gz');
+materialID = int32(prostatelabel.img);
+materialID(materialID == 0  ) = 1;
+materialID(materialID == 4  ) = 1;
+materialID(materialID == 5  ) = 1;
+
+[npixelx, npixely, npixelz] = size(materialID);
+spacingX = prostatelabel.hdr.dime.pixdim(2)*1.e-3;
+spacingY = prostatelabel.hdr.dime.pixdim(3)*1.e-3;
+spacingZ = prostatelabel.hdr.dime.pixdim(4)*1.e-3;
+idslice = 11;
 handle5 = figure(5);
-imagesc(materialID(:,:,npixel/2),[0 3])
+imagesc(materialID(:,:,idslice ),[0 4])
 colorbar
+
 
 %% Query the device
 % GPU must be reset on out of bounds errors
@@ -19,9 +29,6 @@ colorbar
 deviceInfo = gpuDevice(1);
 numSMs = deviceInfo.MultiprocessorCount;
 
-spacingX = 1.0e-3;
-spacingY = 1.0e-3;
-spacingZ = 1.0e-3;
 
 %% mueff at each wave length
 %% TODO - error check same length
@@ -33,14 +40,16 @@ sigmamueff = 30;
 %% Setup Material Parameters
 ntissue = 4;
 nsource    = 10;
-xloc       = npixel/2*spacingX+spacingX*linspace(1,nsource ,nsource )+1.e-4;
-yloc       = npixel/2*spacingY+spacingY*linspace(1,nsource ,nsource )+1.e-4;
-zloc       = npixel/2*spacingZ+spacingZ*linspace(1,nsource ,nsource )+1.e-4;
+idxpix =250; idypix =200;
+xloc       = idxpix *spacingX+spacingX*linspace(1,nsource ,nsource )+1.e-3;
+yloc       = idypix *spacingY+spacingY*linspace(1,nsource ,nsource )+1.e-3;
+zloc       = idslice  *spacingZ+spacingZ*linspace(1,nsource ,nsource )+1.e-3;
+zloc       = idslice  *spacingZ*ones(1,10)+1.e-3;
 power      = 10.;
 
 %% initialize data arrays
 % initialize on host and perform ONE transfer from host to device
-h_pasource     = zeros(npixel,npixel,npixel);
+h_pasource     = zeros(npixelx,npixely,npixelz);
 d_pasource      = gpuArray( h_pasource  );
 
 %% Compile and setup thread grid
@@ -53,14 +62,14 @@ ssptx.GridSize=[numSMs*32               1];
 
 %% Run on GPU
 mueff      = [5.e02 , 4.e02 , 3.e02];
-[d_pasource ] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixel,npixel,npixel);
+[d_pasource ] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixelx,npixely,npixelz);
 
 %%  transfer device to host
 h_pasource  = gather( d_pasource  );
 
 %%  plot pasource
 handle6 = figure(6);
-imagesc(h_pasource(:,:,50), [0 2.e5]);
+imagesc(h_pasource(:,:,idslice), [0 2.e5]);
 colormap default
 colorbar
 
@@ -71,13 +80,14 @@ mixinggrid = [.2, .4, .6, .8]
 tic
 statfigurenames = {'meanpasourcelo','varpasourcelo','meanpasourcehi','varpasourcehi'}
 for idLambda = 1:numel(mueffHbO2) 
+%for idLambda = 1:1
   %% UQ grid 
   HHbgrid  =  sigmamueff * [-2,-1,0,1,2] + mueffHHb(idLambda)
   HbO2grid =  sigmamueff * [-2,-1,0,1,2] + mueffHbO2(idLambda)
   TotalIter = numel(mixinggrid) * numel(mixinggrid) * numel(mixinggrid) * numel(HHbgrid) * numel(HbO2grid);
   IterCount = 0; 
   % mean
-  d_pasourceMean  = gpuArray( zeros(npixel,npixel,npixel)  );
+  d_pasourceMean  = gpuArray( zeros(npixelx,npixely,npixelz)  );
   for idOne = 1: numel(mixinggrid)
     % show iterations
     disp(sprintf('iter %d',idOne));
@@ -92,7 +102,7 @@ for idLambda = 1:numel(mueffHbO2)
              if mod(IterCount,100 )==0
                disp(sprintf(' %d %f %f %f ', IterCount, mueff));
              end
-             [p_pasource] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixel,npixel,npixel);
+             [p_pasource] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixelx,npixely,npixelz);
              d_pasourceMean  = d_pasourceMean  + 1./TotalIter * p_pasource;
   end
   end
@@ -101,7 +111,7 @@ for idLambda = 1:numel(mueffHbO2)
   end
   IterCount = 0; 
   % variance
-  d_pasourceVar  = gpuArray( zeros(npixel,npixel,npixel)  );
+  d_pasourceVar  = gpuArray( zeros(npixelx,npixely,npixelz)  );
   for idOne = 1: numel(mixinggrid)
     % show iterations
     disp(sprintf('iter %d',idOne));
@@ -116,7 +126,7 @@ for idLambda = 1:numel(mueffHbO2)
              if mod(IterCount,100 )==0
                disp(sprintf(' %d %f %f %f ', IterCount, mueff));
              end
-             [p_pasource] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixel,npixel,npixel);
+             [p_pasource] = feval(ssptx,ntissue,materialID, mueff, nsource, power ,xloc,yloc,zloc, d_pasource,spacingX,spacingY,spacingZ,npixelx,npixely,npixelz);
              d_pasourceVar= d_pasourceVar + 1./(TotalIter-1) * (p_pasource - d_pasourceMean  ).*(p_pasource - d_pasourceMean  );
  end
  end
@@ -130,19 +140,23 @@ for idLambda = 1:numel(mueffHbO2)
  % plot
  idplot  =  2*(idLambda-1) +1;
  handleid = figure( idplot   );
- imagesc(host_Mean(:,:,50), [0 2.e+05]);
+ imagesc(host_Mean(:,:,idslice), [0 2.e+05]);
  colormap default
  colorbar
  saveas(handleid,statfigurenames{idplot },'png')
+ prostatedata.img =host_Mean; prostatedata.hdr.dime.datatype=64;
+ save_untouch_nii(prostatedata, [statfigurenames{idplot },'.nii.gz']);
 
  % plot
  idplot =  2*idLambda;
  handleid = figure(  idplot );
  % gather
- imagesc(host_Std(:,:,50), [0 1.e5]);
+ imagesc(host_Std(:,:,idslice), [0 1.e5]);
  colormap default
  colorbar
  saveas(handleid,statfigurenames{idplot },'png')
+ prostatedata.img =host_Std; prostatedata.hdr.dime.datatype=64;
+ save_untouch_nii(prostatedata, [statfigurenames{idplot },'.nii.gz']);
 end
 toc
 
