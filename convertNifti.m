a0f9703193f898e16c7cad4ed59ff2b224c73812
David Fuentes <fuentesdt@gmail.com>
2015-05-28 12:55:14 -0500

    BUG: convert resample

--- convertNifti.m
+++ convertNifti.m
@@ -17,7 +17,6 @@ BMSpacing = [ exampledata.DepthBM(2) -   exampledata.DepthBM(1),...
 %save bmode data
 bmodenii = make_nii(exampledata.BM,BMSpacing)
 save_nii(bmodenii,'bmode.nii.gz') 
-savevtkcmd = 'c3d  bmode.nii.gz -o bmode.vtk'
 
 %save pa data at each wavelength
 for iii = 1:size(exampledata.Wave,2)
@@ -28,6 +27,7 @@ for iii = 1:size(exampledata.Wave,2)
 end
 
 resamplecmd = '/opt/apps/ANTsR/dev//ANTsR_src/ANTsR/src/ANTS/ANTS-build//bin/WarpImageMultiTransform 3 bmode.nii.gz bmoderesample.nii.gz identity.txt -R padata1.nii.gz'
+savevtkcmd  = 'c3d  bmoderesample.nii.gz -o bmoderesample.vtk'
 maskcmd     = '/opt/apps/itksnap/c3d-1.0.0-Linux-x86_64/bin/c3d -verbose  bmoderesample.nii.gz -thresh 50 inf 1 0 -connected-components -threshold 1 1 1 0 -dilate 1 2x2x2vox -erode 1 2x2x2vox  -o bmodemask.nii.gz'
 segmentationcmd = '/opt/apps/ANTsR/dev//ANTsR_src/ANTsR/src/ANTS/ANTS-build//bin/Atropos -d 3 -a bmoderesample.nii.gz  -x bmodemask.nii.gz -i kmeans[3]  -c [3,0.0] -m [0.1,1x1x1] -k Gaussian -o [bmode_ATROPOS_GMM.nii.gz,bmode_ATROPOS_GMM_POSTERIORS%d.nii.gz]'
 
